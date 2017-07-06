94a7f7201295293ca25072061145902e5730d871
jaydeshow <jaydeshow@gmail.com>
2017-07-06 11:23:47 +0800

    alt-enter code complete產生  call back listener

--- androidadv/Fragement1/app/src/main/java/com/chtti/fragement1/MainActivity.java
+++ androidadv/Fragement1/app/src/main/java/com/chtti/fragement1/MainActivity.java
@@ -2,12 +2,21 @@ package com.chtti.fragement1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToggleButton button = (ToggleButton) findViewById(R.id.toggleButton);
+        button.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
     }
 }
